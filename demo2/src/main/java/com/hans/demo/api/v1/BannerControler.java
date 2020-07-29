554ef36209aac32a0749ef529ff58ae6cf3f8853
Your Name <11091752@vivo.com>
2020-07-29 20:45:02 +0800

    java自定义返回用户信息

--- demo2/src/main/java/com/hans/demo/api/v1/BannerControler.java
+++ demo2/src/main/java/com/hans/demo/api/v1/BannerControler.java
@@ -1,28 +1,46 @@
 package com.hans.demo.api.v1;
 
+import com.hans.demo.dto.PersonDTO;
+import com.hans.demo.exception.http.ForbiddenException;
 import com.hans.demo.exception.http.NotFoundException;
 import com.hans.demo.sample.ISkill;
 import com.hans.demo.sample.hero.Diana;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 
+@Validated
 @RestController
 @RequestMapping("/banner")
 public class BannerControler {
     @Autowired
     private ISkill iSkill;
 
+    // private PersonDTO dto;
+
     @GetMapping("/test")
     public String test() {
+        System.out.println("hahah");
         iSkill.r();
 
         throw new NotFoundException(10000);
-//        return "hello,world3!";
+    }
+
+
+    @PostMapping("/test2/{id}")
+    public PersonDTO test2(
+            @PathVariable @Range(min = 1, max = 10, message = "长度错误") Integer id,
+            @RequestParam @Length(min = 8) String name,
+            @RequestBody @Validated PersonDTO person) {
+        iSkill.r();
+        PersonDTO dto = PersonDTO.builder().name("7").age(18).build();
+
+        return dto;
+        // throw new ForbiddenException(10000);
     }
 }
--- demo2/src/main/java/com/hans/demo/core/GlobalExceptionAdvice.java
+++ demo2/src/main/java/com/hans/demo/core/GlobalExceptionAdvice.java
@@ -6,15 +6,21 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.xml.ws.http.HTTPException;
 
 import com.hans.demo.exception.http.HttpException;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionAdvice {
@@ -45,5 +51,38 @@ public class GlobalExceptionAdvice {
         UnifyResponse message = new UnifyResponse(e.getCode(), codeConfiguration.getMessage(e.getCode()), method + " " + requestUrl);
         ResponseEntity<UnifyResponse> r = new ResponseEntity<>(message, headers, httpStatus);
         return r;
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
+    public UnifyResponse handleBeanValidation(HttpServletRequest req, MethodArgumentNotValidException e) {
+        String requestUrl = req.getRequestURI();
+        String method = req.getMethod();
+
+        List<ObjectError> errors = e.getBindingResult().getAllErrors();
+        String message = this.formatAllErrorMessages(errors);
+        return new UnifyResponse(10001, message, method + " " + requestUrl);
+    }
+
+    public String formatAllErrorMessages(List<ObjectError> errors) {
+        StringBuffer errorMsg = new StringBuffer();
+        errors.forEach(error -> errorMsg.append(error.getDefaultMessage()).append(";"));
+        return errorMsg.toString();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public UnifyResponse handleConstraintException(HttpServletRequest req, ConstraintViolationException e) {
+        String requestUrl = req.getRequestURI();
+        String method = req.getMethod();
+        String message = e.getMessage();
+
+        return new UnifyResponse(10001, message, method + " " + requestUrl);
+//        for(ConstraintViolation error: e.getConstraintViolations()){
+//            ConstraintViolation a = error;
+//        }
+//        return null;
     }
 }
