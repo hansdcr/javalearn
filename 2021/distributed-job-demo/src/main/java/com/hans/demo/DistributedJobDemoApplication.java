53925aef8ee3986a39746f0a67c33d985df1fc16
李世海 <lishihai@lishihaideMacBook-Pro.local>
2021-04-11 20:26:34 +0800

    distributed-job-demo

---
+++ 2021/distributed-job-demo/src/main/java/com/hans/demo/DistributedJobDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hans.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedJobDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedJobDemoApplication.class, args);
+    }
+
+}
